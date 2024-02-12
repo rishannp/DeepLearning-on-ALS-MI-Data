0566a8fac1d7d4eac3237478666ca052d4888711
Rishan Patel <76795183+rishannp@users.noreply.github.com>
2024-02-12 22:47:57 +0000

    Add files via upload

--- one.py
+++ one.py
@@ -78,7 +78,7 @@ def makespect(S1):
     y = np.concatenate((yl,yr,yre),3)
     return img,y
 
-
+# I have had to prepad the third dimension with zeros as not all classes or instances are the same time length. Therefore many images will show some no activity areas. 
 S1,y = makespect(subject_data['S1'])
 # S2 = makespect(subject_data['S2'])
 # S5 = makespect(subject_data['S5'])
@@ -90,35 +90,33 @@ S1,y = makespect(subject_data['S1'])
 
 
 #%%
+x = S1[:,:,:,1]
+# RAW SPECTROGRAM
+mel_spec = librosa.feature.melspectrogram(y=x, sr=256, hop_length=np.size(x,1)//64, 
+                                          n_fft=256, n_mels=64, fmin=0, fmax=100, win_length=16)
 
-
-# idx = ['L','R','Re']
-# x = np.transpose(S1[idx[2]][0,44])
-
-# # RAW SPECTROGRAM
-# mel_spec = librosa.feature.melspectrogram(y=x, sr=256, hop_length=np.size(x,1)//64, 
-#                                           n_fft=256, n_mels=64, fmin=0, fmax=100, win_length=128)
-
-# # The output of mel_spec is 22x64x68. That is Channels X Mel Bin Height X Number of Frames
-# # Number of frames is calculated by (size(x,1)-n_fft) // hop_length + 1. so 1080-256 // 16+1 = 68
+# The output of mel_spec is 22x64x68. That is Channels X Mel Bin Height X Number of Frames
+# Number of frames is calculated by (size(x,1)-n_fft) // hop_length + 1. so 1080-256 // 16+1 = 68
  
-# # LOG TRANSFORM
-# width = (mel_spec.shape[1]//32)*32
-# mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max).astype(np.float32)[:,:width]
+# LOG TRANSFORM
+width = (mel_spec.shape[1]//32)*32
+mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max).astype(np.float32)[:,:width]
 
-# # STANDARDIZE TO -1 TO 1
-# mel_spec_db = (mel_spec_db+40)/40 
+# STANDARDIZE TO -1 TO 1
+mel_spec_db = (mel_spec_db+40)/40 
    
 
-# plt.figure()
-# plt.imshow(mel_spec_db[0,:,:])
-# plt.colorbar()
+plt.figure()
+plt.imshow(mel_spec_db[:,:,0,0])
+plt.colorbar()
 # # np.savez("spectrogram_data.npz", S1=S1, S2=S2, S5=S5, S9=S9, S21=S21, S31=S31, S34=S34, S39=S39)
 # %% Data Augmentation
 
+
+ 
 # %% Data splitting
 
-def split(S1, y, train_percentage):
+def split1(S1, y, train_percentage):
     # Shuffle the indices along the 4th dimension
     num_samples = np.size(S1, 3)
     indices = np.arange(num_samples)
@@ -140,22 +138,53 @@ def split(S1, y, train_percentage):
     return train_S1, train_y, test_S1, test_y
 
 
-train_S1, train_y, test_S1, test_y = split(S1, y, 0.3)
+train_S1, train_y, test_S1, test_y = split1(S1, y, 0.3)
 del S1,y
 
-# %%
+# %% 
 
+def split(S1, y, train_percentage):
+    # Shuffle the indices along the 4th dimension
+    num_samples = np.size(S1, 3)
+    indices = np.arange(num_samples)
+    np.random.shuffle(indices)
+
+    # Shuffle both S1 and y using the shuffled indices
+    S1_shuffled = S1[:, :, :, indices]
+    y_shuffled = y[:, :, :, indices]
+    
+    # Calculate the number of samples for training
+    num_train_samples = int(train_percentage * num_samples)
+    
+    # Split the shuffled data into training and testing sets
+    train_S1 = S1_shuffled[:, :, :, :num_train_samples]
+    train_y = y_shuffled[:, :, :, :num_train_samples]
+    test_S1 = S1_shuffled[:, :, :, num_train_samples:]
+    test_y = y_shuffled[:, :, :, num_train_samples:]
+
+    # Add an extra dimension of size 1 to each array
+    train_S1 = np.expand_dims(train_S1, axis=-1)
+    train_y = np.expand_dims(train_y, axis=-1)
+    test_S1 = np.expand_dims(test_S1, axis=-1)
+    test_y = np.expand_dims(test_y, axis=-1)
+
+    return train_S1, train_y, test_S1, test_y
+
+train_S1, train_y, test_S1, test_y = split(S1, y, 0.3)
+del S1, y
+
+# %%
 
 # %% Data Loader 
 # Dataloader is jarring asf so you need to have the sample dimension as the first, so i need to reshape it as follows:
 
-# Reshape train_S1 and train_y to have a shape of (n, 22, 64, 156)
+# Reshape train_S1 and train_y to have a shape of (n, 22, 64, 156,1)
 # Transpose train_S1 and train_y
-train_S1 = np.transpose(train_S1, (3, 0, 1, 2))  # Move the sample dimension to the first dimension
-train_y = np.transpose(train_y, (3, 0, 1, 2))    # Adjust the dimensions as needed for labels
+train_S1 = np.transpose(train_S1, (3, 0, 1, 2,4))  # Move the sample dimension to the first dimension
+train_y = np.transpose(train_y, (3, 0, 1, 2,4))    # Adjust the dimensions as needed for labels
 # Transpose test_S1 and test_y (assuming you intended to transpose test_S1)
-test_S1 = np.transpose(test_S1, (3, 0, 1, 2))  # Move the sample dimension to the first dimension
-test_y = np.transpose(test_y, (3, 0, 1, 2))    # Adjust the dimensions as needed for labels
+test_S1 = np.transpose(test_S1, (3, 0, 1, 2,4))  # Move the sample dimension to the first dimension
+test_y = np.transpose(test_y, (3, 0, 1, 2,4))    # Adjust the dimensions as needed for labels
 
 
 train_loader = tf.data.Dataset.from_tensor_slices((train_S1, train_y))
@@ -170,57 +199,95 @@ train_dataset = (
 validation_dataset = (
     validation_loader.prefetch(2)
 )
+
+# %% Test take batch 
+
+# for batch in train_dataset.take(1):
+#     # 'batch' will be a tuple containing input data and labels
+#     input_data, labels = batch
+
+#     # Print the shapes of input data and labels
+#     print("Input data shape:", input_data.shape)
+#     print("Labels shape:", labels.shape)
+    
+
 # %% Model loading
 
 def get_model(width=22, height=64, depth=156):
     """Build a 3D convolutional neural network model."""
 
     # inputs = keras.Input((width, height, depth, 1))
-
-    # x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(inputs)
-    # x = layers.MaxPool3D(pool_size=2)(x)
+    
+    # x = layers.Conv3D(filters=32, kernel_size=(3, 3, 3), activation="relu")(inputs)
+    # x = layers.MaxPool3D(pool_size=(2, 2, 2))(x)
     # x = layers.BatchNormalization()(x)
-
-    # x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(x)
-    # x = layers.MaxPool3D(pool_size=2)(x)
+    
+    # x = layers.Conv3D(filters=64, kernel_size=(3, 3, 3), activation="relu")(x)
+    # x = layers.MaxPool3D(pool_size=(2, 2, 2))(x)
     # x = layers.BatchNormalization()(x)
-
-    # x = layers.Conv3D(filters=128, kernel_size=3, activation="relu")(x)
-    # x = layers.MaxPool3D(pool_size=2)(x)
+    
+    # x = layers.Conv3D(filters=128, kernel_size=(3, 3, 3), activation="relu")(x)
+    # x = layers.MaxPool3D(pool_size=(2, 2, 2))(x)
     # x = layers.BatchNormalization()(x)
-
-    # x = layers.Conv3D(filters=256, kernel_size=3, activation="relu")(x)
-    # x = layers.MaxPool3D(pool_size=2)(x)
+    
+    # x = layers.Conv3D(filters=256, kernel_size=(3, 3, 3), activation="relu")(x)
+    # x = layers.MaxPool3D(pool_size=(2, 2, 2))(x)
     # x = layers.BatchNormalization()(x)
-
+    
     # x = layers.GlobalAveragePooling3D()(x)
     # x = layers.Dense(units=512, activation="relu")(x)
     # x = layers.Dropout(0.3)(x)
-
-    # outputs = layers.Dense(units=1, activation="sigmoid")(x)
-    inputs = keras.Input((width, height, depth, 1))
     
+
+    # Define the input shape
+    input_shape = (None, 22, 64, 156, 1)
+    
+    # Define the input layer
+    inputs = layers.Input(input_shape)
+    
+    # Convolutional layers
     x = layers.Conv3D(filters=32, kernel_size=(3, 3, 3), activation="relu")(inputs)
+    # Input shape: (22, 64, 156, 1), Output shape: (20, 62, 154, 32)
+    
     x = layers.MaxPool3D(pool_size=(2, 2, 2))(x)
+    # Input shape: (20, 62, 154, 32), Output shape: (10, 31, 77, 32)
+    
     x = layers.BatchNormalization()(x)
     
     x = layers.Conv3D(filters=64, kernel_size=(3, 3, 3), activation="relu")(x)
+    # Input shape: (10, 31, 77, 32), Output shape: (8, 29, 75, 64)
+    
     x = layers.MaxPool3D(pool_size=(2, 2, 2))(x)
+    # Input shape: (8, 29, 75, 64), Output shape: (4, 14, 37, 64)
+    
     x = layers.BatchNormalization()(x)
     
     x = layers.Conv3D(filters=128, kernel_size=(3, 3, 3), activation="relu")(x)
-    x = layers.MaxPool3D(pool_size=(2, 2, 2))(x)
+    # Input shape: (4, 14, 37, 64), Output shape: (2, 12, 35, 128)
+    
     x = layers.BatchNormalization()(x)
     
-    x = layers.Conv3D(filters=256, kernel_size=(3, 3, 3), activation="relu")(x)
-    x = layers.MaxPool3D(pool_size=(2, 2, 2))(x)
+    x = layers.Conv3D(filters=256, kernel_size=(1, 1, 1), activation="relu")(x)
+    # Input shape: (2, 12, 35, 128), Output shape: (2, 12, 35, 256)
+    
+    # Remove the MaxPooling3D layer here
+    
     x = layers.BatchNormalization()(x)
     
+    # Global average pooling layer
     x = layers.GlobalAveragePooling3D()(x)
+    # Input shape: (2, 12, 35, 256), Output shape: (256,)
+    
+    # Fully connected layers
     x = layers.Dense(units=512, activation="relu")(x)
+    # Input shape: (256,), Output shape: (512,)
+    
+    # Dropout layer
     x = layers.Dropout(0.3)(x)
     
-    outputs = layers.Dense(units=1, activation="sigmoid")(x)
+    # Output layer
+    outputs = layers.Dense(units=3, activation="softmax")(x)
+    # Input shape: (512,), Output shape: (3,)
 
     # Define the model.
     model = keras.Model(inputs, outputs, name="3dcnn")
@@ -238,7 +305,7 @@ lr_schedule = keras.optimizers.schedules.ExponentialDecay(
     initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
 )
 model.compile(
-    loss="binary_crossentropy",
+    loss="categorical_crossentropy",
     optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
     metrics=["acc"],
     run_eagerly=True,
